2447f8c2deb0a98929ee212a189375bbbbd07e13
Myles Lewis <myles.lewis@qmul.ac.uk>
2024-06-12 08:44:12 +0100

    Create gene2symbol

---
+++ R/gene2symbol.R
@@ -0,0 +1,42 @@
+
+#' Converts ensembl gene ids to symbols 
+#'
+#' Uses a loaded ensembl database to convert ensembl gene ids to symbol. If a
+#' vector is provided, a vector of symbols is returned. If a cellMarkers object
+#' is provided, the rownames in the genemeans, genemeans_filtered, groupmeans
+#' and groupmeans_filtered elements are changed to symbol and the cellMarkers
+#' object is returned.
+#' 
+#' @param x Either a vector of ensembl gene ids to convert or a 'cellMarkers'
+#'   class object.
+#' @param ensdb An ensembl database object loaded via the `AnnotationHub`
+#'   bioconductor package.
+#' @returns If `x` is a vector, a vector of symbols is returned. If no symbol is
+#'   no available the ensembl id is left untouched. If `x` is a 'cellMarkers'
+#'   class object, a 'cellMarkers' object is returned with rownames in the
+#'   results elements converted to gene symbols.
+#' @export
+
+gene2symbol <- function(x, ensdb) {
+  if (inherits(x, "cellMarker")) {
+    rownames(x$genemeans) <- convertsymbol(rownames(x$genemeans))
+    rownames(x$genemeans_filtered) <- convertsymbol(rownames(x$genemeans_filtered))
+    rownames(x$groupmeans) <- convertsymbol(rownames(x$groupmeans))
+    rownames(x$groupmeans_filtered) <- convertsymbol(rownames(x$groupmeans_filtered))
+    return(x)
+  } else {
+    convertsymbol(x)
+  }
+}
+
+
+#' @importFrom ensembldb select
+#' 
+convertsymbol <- function(x, ensdb) {
+  geneid <- ensembldb::select(ensdb, keys = x, keytype = "GENEID",
+                              columns = c("GENEID", "SYMBOL"))
+  ok <- geneid$SYMBOL != "" & !is.na(geneid$SYMBOL)
+  x[ok] <- geneid$SYMBOL[ok]
+  attr(x, "ok") <- ok
+  x
+}
