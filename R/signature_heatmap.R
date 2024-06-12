5c67c603539817a8625299ed37bf87dbd5458078
Myles Lewis <myles.lewis@qmul.ac.uk>
2024-06-12 08:15:03 +0100

    Add sphere scaling to heatmap

--- R/signature_heatmap.R
+++ R/signature_heatmap.R
@@ -14,8 +14,9 @@
 #' @param rank Either "max" or "angle" controlling whether genes (rows) are
 #'   ordered in the heatmap by max expression (the default) or lowest angle
 #'   (a measure of specificity of the gene as a cell marker).
-#' @param scale Logical whether to scale genes to equalise the maximum mean
-#'   expression between genes.
+#' @param scale Character value controlling scaling of genes: "none" for no scaling, "max" to equalise the maximum mean
+#'   expression between genes, "sphere" to scale genes to the unit hypersphere
+#'   where cell subclasses or groups are dimensions.
 #' @param col Vector of colours passed to [ComplexHeatmap::Heatmap()].
 #' @param ... Optional arguments passed to [ComplexHeatmap::Heatmap()].
 #' @returns A 'Heatmap' class object.
@@ -28,11 +29,12 @@ signature_heatmap <- function(x,
                               type = c("subclass", "group"),
                               use_filter = NULL,
                               rank = c("max", "angle"),
-                              scale = FALSE,
+                              scale = c("none", "max", "sphere"),
                               col = rev(hcl.colors(10, "Greens3")),
                               ...) {
-  rank <- match.arg(rank)
   type <- match.arg(type)
+  rank <- match.arg(rank)
+  scale <- match.arg(scale)
   cell_table <- NULL
   if (inherits(x, "deconv")) {
     x <- x$mk
@@ -60,7 +62,8 @@ signature_heatmap <- function(x,
     ord <- seq_len(nrow(gene_signature))
   }
   rs <- cell_table[whmax]
-  if (scale) gene_signature <- gene_signature / rmax
+  if (scale == "max") gene_signature <- gene_signature / rmax
+  if (scale == "sphere") gene_signature <- scaleSphere(gene_signature)
   
   Heatmap(gene_signature,
           cluster_rows = FALSE,
