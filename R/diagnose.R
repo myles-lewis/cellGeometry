5b98f661ecf6f70089711ef6a843a2f614a5698b
Myles Lewis <myles.lewis@qmul.ac.uk>
2024-06-16 11:59:58 +0100

    Update diagnose.R

--- R/diagnose.R
+++ R/diagnose.R
@@ -22,29 +22,31 @@ diagnose <- function(mk) {
     sum(ranks == 1)
   }, numeric(1))
   w <- which(no1 < 5)
+  
   if (length(w) > 0) {
+    s1 <- mk$spillover
+    spmax <- vapply(w, function(i) {
+      col_i <- s1[, i]
+      col_i[i] <- 0
+      wmax <- which.max(col_i)
+      c(max(col_i), wmax)
+    }, numeric(2))
+    w <- w[order(spmax[1, ], decreasing = TRUE)]
+    spmax <- spmax[, order(spmax[1, ], decreasing = TRUE)]
+    
     cat("Weak subclass signatures:\n")
     cat(paste(paste0(format(colnames(mk$genemeans)[w], justify = "left"),
                      "  ", no1[w], "/", nsubclass),
               collapse = "\n"))
-    cat("\n")
+    cat("\n\n")
+    
+    s1w <- format(colnames(s1)[w], justify = "left")
+    spmaxname <- format(colnames(s1)[spmax[2, ]], justify = "left")
+    s1set <- paste(s1w, " spills most into ", spmaxname, "",
+                   format(spmax[1, ], digits = 3))
+    cat(paste(s1set, collapse = "\n"))
+    cat("\n\n")
   }
-  
-  cat("\n")
-  s1 <- mk$spillover
-  spmax <- vapply(w, function(i) {
-    x <- colnames(s1)[i]
-    col_i <- s1[, i]
-    col_i[i] <- 0
-    wmax <- which.max(col_i)
-    spmax <- colnames(s1)[wmax]
-    c(spmax, format(max(col_i), digits = 3))
-  }, character(2))
-  s1w <- format(colnames(s1)[w], justify = "left")
-  spmaxname <- format(spmax[1, ], justify = "left")
-  s1set <- paste(s1w, " spills most into ", spmaxname, "", spmax[2, ])
-  cat(paste(s1set, collapse = "\n"))
-  cat("\n\n")
   
   no1 <- vapply(mk$group_angle, function(i) {
     ranks <- i[seq_len(ngroup), "rank"]
