fd19ada123165cd03546388441b4ef1fccdb0998
Myles Lewis <myles.lewis@qmul.ac.uk>
2024-09-07 19:01:46 +0100

    Initial build mergeMarkers

---
+++ R/mergeMarkers.R
@@ -0,0 +1,40 @@
+
+#' @export
+mergeMarkers <- function(mk1, mk2,
+                         remove_subclass = NULL,
+                         remove_group = NULL, ...) {
+  .call <- match.call()
+  if (!inherits(mk1, "cellMarkers")) stop("'mk1' is not a 'cellMarkers' object")
+  if (!inherits(mk2, "cellMarkers")) stop("'mk2' is not a 'cellMarkers' object")
+  
+  common <- intersect(rownames(mk1$genemeans), rownames(mk2$genemeans))
+  message(length(common), " common genes")
+  gm1 <- mk1$genemeans[common, ]
+  gm2 <- mk2$genemeans[common, ]
+  genemeans <- cbind(gm1, gm2)
+  if (any(remove_subclass %in% colnames(genemeans))) {
+    genemeans <- genemeans[, !colnames(genemeans) %in% remove_subclass]
+  }
+  
+  cell_table <- c(mk1$cell_table, mk2$cell_table)
+  cell_table <- cell_table[!names(cell_table) %in% remove_subclass]
+  old_levels <- levels(cell_table)
+  cell_table <- droplevels(cell_table)
+  gone <- setdiff(old_levels, levels(cell_table))
+  if (is.null(remove_group)) remove_group <- gone
+  
+  gp1 <- mk1$groupmeans[common, ]
+  gp2 <- mk2$groupmeans[common, ]
+  groupmeans <- cbind(gp1, gp2)
+  if (any(remove_group %in% colnames(groupmeans))) {
+    groupmeans <- groupmeans[, !colnames(groupmeans) %in% remove_group]
+  }
+  
+  mk1$genemeans <- genemeans
+  mk1$groupmeans <- groupmeans
+  mk1$cell_table <- cell_table
+  mk1$subclass_table <- c(mk1$subclass_table, mk2$subclass_table)
+  mk1$subclass_table[!names(mk1$subclass_table) %in% remove_subclass]
+  
+  updateMarkers(mk1)
+}
