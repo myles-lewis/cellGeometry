b83c0f554450fb021c6db69f1ac6d602bce14b25
Myles Lewis <myles.lewis@qmul.ac.uk>
2024-10-02 22:46:23 +0100

    Add eta

--- R/txtProgressBar2.R
+++ R/txtProgressBar2.R
@@ -5,7 +5,7 @@
 #' @importFrom utils flush.console setTxtProgressBar
 
 txtProgressBar2 <- function(min = 0, max = 1, initial = 0, char = "=",
-                            width = NA, title = "") {
+                            width = NA, title = "", eta = TRUE) {
   .val <- initial
   .killed <- FALSE
   .nb <- 0L
@@ -31,10 +31,17 @@ txtProgressBar2 <- function(min = 0, max = 1, initial = 0, char = "=",
     pc <- round(100 * (value - min)/(max - min))
     if (nb == .nb && pc == .pc) 
       return()
+    if (eta) {
+      curr <- Sys.time()
+      dur <- as.numeric(difftime(curr, .start, units = "secs"))
+      rem <- format_dur((100 - pc) / pc * dur)
+      tim <- if (pc != 100) paste("  eta", rem) else ""
+      tim <- str_pad(tim, 15)
+    }
     cat(paste0("\r", title, "  |", strrep(" ", width + 6)))
-    cat(paste(c("\r", title, "  |", rep.int(char, nb),
-                rep.int(" ", nw * (width - nb)),
-                sprintf("| %3d%%", pc)), collapse = ""))
+    cat(paste(c("\r", title, "  |", strrep(char, nb),
+                strrep(" ", nw * (width - nb)),
+                sprintf("| %3d%%", pc), tim), collapse = ""))
     flush.console()
     .nb <<- nb
     .pc <<- pc
@@ -51,4 +58,28 @@ txtProgressBar2 <- function(min = 0, max = 1, initial = 0, char = "=",
   .start <- Sys.time()
   structure(list(getVal = getVal, up = up, kill = kill),
             class = "txtProgressBar")
+}
+
+
+format_dur <- function(x) {
+  x <- as.numeric(x)
+  s <- x %% 60
+  m <- floor(x / 60)
+  h <- floor(m / 60)
+  if (h > 0) {
+    m <- m %% 60
+    return(paste0(h, "h ", m, "m"))
+  }
+  if (m > 0) {
+    return(paste0(m, "m ", floor(s), "s"))
+  }
+  s <- format(round(s, 1), digits = 2)
+  paste0(s, " secs")
+}
+
+
+str_pad <- function(x, n) {
+  nc <- nchar(x)
+  if (nc < n) x <- paste0(x, strrep(" ", n - nc))
+  x
 }
