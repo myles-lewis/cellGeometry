510440158d429cfee758e362b49f22f96e3f3e58
Myles Lewis <myles.lewis@qmul.ac.uk>
2025-04-04 16:59:11 +0100

    Update Rd

--- R/simulate_bulk.R
+++ R/simulate_bulk.R
@@ -79,7 +79,7 @@ generate_samples <- function(object, n, equal_sample = TRUE,
 #'   columns. This can be used as `test` with the [deconvolute()] function.
 #' @seealso [generate_samples()] [deconvolute()] [add_noise()]
 #' @export
-simulate_bulk <- function(object, samples, subclass, times = 30,
+simulate_bulk <- function(object, samples, subclass, times = 10,
                           method = c("unif", "dirichlet"), alpha = 1) {
   if (inherits(object, "cellMarkers")) {
     genemean_counts <- 2^object$genemeans -1
