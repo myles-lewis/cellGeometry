3ec0fc2ba6a66469235479d6c6291a5ed94d13ec
Myles Lewis <myles.lewis@qmul.ac.uk>
2025-05-22 21:47:21 +0100

    Bugfix fix_bulk

--- R/fix_bulk.R
+++ R/fix_bulk.R
@@ -16,11 +16,11 @@ fix_bulk <- function(mk, bulk) {
   genes <- unique(c(mk$geneset, mk$group_geneset))
   ok <- genes %in% rownames(bulk)
   if (all(ok)) return(bulk)
-  pc <- format(sum(!ok) / length(genes), digits = 2)
+  pc <- format(sum(!ok) / length(genes) *100, digits = 2)
   message(sum(!ok), "/", length(genes), " (", pc, 
           "%) signature genes missing from bulk")
   miss <- genes[!ok]
   extra <- matrix(0, nrow = length(miss), ncol = ncol(bulk))
   rownames(extra) <- miss
-  rbind(bulk, extra)
+  rbind(as.matrix(bulk), extra)
 }
