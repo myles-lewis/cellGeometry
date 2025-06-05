40717ab6d9601043b7685c2e64b725de9bf256d3
Myles Lewis <myles.lewis@qmul.ac.uk>
2025-06-05 11:40:30 +0100

    Change default times = 1

--- R/simulate_bulk.R
+++ R/simulate_bulk.R
@@ -84,7 +84,7 @@ generate_samples <- function(object, n, equal_sample = TRUE,
 #'   columns. This can be used as `test` with the [deconvolute()] function.
 #' @seealso [generate_samples()] [deconvolute()] [add_noise()]
 #' @export
-simulate_bulk <- function(object, samples, subclass, times = 10,
+simulate_bulk <- function(object, samples, subclass, times = 1,
                           method = c("dirichlet", "unif"), alpha = 1) {
   if (inherits(object, "cellMarkers")) {
     genemean_counts <- 2^object$genemeans -1
